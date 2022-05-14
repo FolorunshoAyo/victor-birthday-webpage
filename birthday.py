3b766ae302e16cdd2f360c8dcaec3c2ae590a5df
Shodiya Folorunsho <folushoayomide11@gmail.com>
2022-05-14 13:36:15 +0100

    Finished with the remaining tab contents

---
+++ birthday.py
@@ -0,0 +1,17 @@
+# HAPPY BIRTHDAY MY BABY!!!!
+import datetime;
+
+your_day = datetime.datetime(2022, 5, 17);
+age = 22;
+
+while True:
+    print("Current age is " + str(age));
+    if(age == 23):
+        print(your_day);
+        print("Your age now is " + str(age));
+        print("Happy Birthday To You!!!");
+    age += 1;
+    # Your age knows no bound. 
+    # Your blessing to me is now and forever
+
+
